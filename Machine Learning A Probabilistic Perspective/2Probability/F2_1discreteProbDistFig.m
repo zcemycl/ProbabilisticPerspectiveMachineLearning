020845012d94e88454c58016fb738f566b4874be
Yui Chun Leung <42243073+zcemycl@users.noreply.github.com>
2019-11-02 15:17:51 +0000

    update continous and discrete

--- Machine Learning A Probabilistic Perspective/2Probability/F2_1discreteProbDistFig.m
+++ Machine Learning A Probabilistic Perspective/2Probability/F2_1discreteProbDistFig.m
@@ -1,12 +1,18 @@
 samples = uniform(1,5,[1000000,1]);
-subplot(121)
+subplot(221)
 histogram(samples,4)
 
-subplot(122)
-histogram(uniform(1,2,[100000,1]),1);
+subplot(222)
+histogram(uniform(1,2,[1000000,1]),1);
 xlim([1,5])
 
+subplot(223)
+histogram(randi([1,4],1000000,1));
+title('discrete')
 
+subplot(224)
+histogram(uniform(1,4,[1000000,1]));
+title('continuous')
 
 function samples = uniform(a,b,shape)
 samples = (b-a).*rand(shape)+a;
